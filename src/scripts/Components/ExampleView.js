115b48f6265b785e676d683d620768d12454b5ac
Nicify <nsnull@protonmail.com>
2019-06-24 02:42:42 +0800

    optmize speed

--- src/scripts/Components/ExampleView.js
+++ src/scripts/Components/ExampleView.js
@@ -11,7 +11,12 @@ export default class ExampleView extends React.PureComponent {
   render() {
     return (
       <>
-        {this.state.showCode ? this.props.code : this.props.demo}
+        <view frame={$rect(0, 0, width, width)} hidden={!this.state.showCode}>
+          {this.props.code}
+        </view>
+        <view frame={$rect(0, 0, width, width)} hidden={this.state.showCode}>
+          {this.props.demo}
+        </view>
         <button
           frame={$rect(width - 50, width - 40, 40, 35)}
           image={codeIcon}
