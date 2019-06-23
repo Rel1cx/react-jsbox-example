91dcb445326ef1b37d56f38c3745490a2a7f3ff2
Nicify <nsnull@protonmail.com>
2019-06-23 20:32:11 +0800

    optimize launch speed

--- src/scripts/Components/ExampleView.js
+++ src/scripts/Components/ExampleView.js
@@ -14,7 +14,7 @@ export default class ExampleView extends React.Component {
         {this.state.showCode ? this.props.code : this.props.demo}
         <button
           frame={$rect(width - 50, width - 40, 40, 35)}
-          image={$data({ url: codeIcon }).image}
+          image={codeIcon}
           bgcolor={$color('clear')}
           events={{
             tapped: () => {
