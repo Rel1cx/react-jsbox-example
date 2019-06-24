69114c824aae6e1e5c856a8e52dc7d11d32edd1a
Nicify <nsnull@protonmail.com>
2019-06-24 23:33:40 +0800

    hotfix

---
+++ src/scripts/Examples/index.js
@@ -0,0 +1,11 @@
+import ClassExample from './ClassExample'
+import ReducerExample from './ReducerExample'
+import CacheExample from './CacheExample'
+import HttpExample from './HttpExample'
+
+export default {
+  ClassExample,
+  ReducerExample,
+  CacheExample,
+  HttpExample
+}
