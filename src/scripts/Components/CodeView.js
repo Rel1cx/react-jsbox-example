7e288cd03cdf3ea3c4b86b1fe9331c2b2904a7a5
Nicify <nsnull@protonmail.com>
2019-06-26 18:11:44 +0800

    优化构建命令，使用 EJS 渲染代码高亮 HTML

--- src/scripts/Components/CodeView.js
+++ src/scripts/Components/CodeView.js
@@ -1,40 +1,9 @@
 import React from 'react'
+import ejs from 'ejs'
 const { width, height } = $device.info.screen
+const HLTemplate = $file.read('assets/prism.ejs').string
 export default function CodeView(props) {
-  const code = props.content.replace(/[\u00A0-\u9999<>&]/gim, i => `&#${i.charCodeAt(0)};`)
-  const html = `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="user-scalable=no" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.16.0/themes/prism-tomorrow.css" />
-      <title>Code</title>
-      <style>
-        * {
-          margin: 0;
-          padding: 0;
-        }
-        body {
-          background: #2d2d2d;
-        }
-        #prism-pre,
-        #prism-code {
-          white-space: pre-wrap;
-          word-wrap: break-word;
-          font-size: 1.2rem;
-        }
-      </style>
-    </head>
-    <body>
-      <pre id="prism-pre" class="language-jsx"><code id="prism-code" class="language-jsx">${code}</code></pre>
-      <script src="https://cdn.jsdelivr.net/npm/prismjs@1.16.0/prism.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/prismjs@1.16.0/components/prism-javascript.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/prismjs@1.16.0/components/prism-jsx.min.js"></script>
-      <script>
-        Prism.highlightAll(true)
-      </script>
-    </body>
-  </html>
-  `
+  const { content } = props
+  const html = ejs.render(HLTemplate, { code: content })
   return <web frame={$rect(0, 0, width, width)} html={html} />
 }
