62817a567dd637ed3a2d6f61470d2c42b9b23bd9
Nicify <nsnull@protonmail.com>
2019-08-09 12:33:54 +0800

    upgrade react to 16.9 stable

--- src/scripts/Components/CodeView.js
+++ src/scripts/Components/CodeView.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { pupa } from "../helper"
+import { pupa } from '../helper'
 
-const { width, height } = $device.info.screen
+const { width } = $device.info.screen
+
 const HLTemplate = $file.read('assets/prism.html').string
+
 export default function CodeView(props) {
   const { content } = props
+
   const html = pupa(HLTemplate, { code: content })
+
   return <web frame={$rect(0, 0, width, width)} html={html} />
 }
