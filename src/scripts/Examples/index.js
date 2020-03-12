b2cd393f46bf6a0c102857684fe84b4a08114da5
Nicify <nsnull@protonmail.com>
2020-03-13 00:43:38 +0800

    feat: add LottieExample

--- src/scripts/Examples/index.js
+++ src/scripts/Examples/index.js
@@ -1,17 +1,17 @@
 import ClassExample from './ClassExample'
-import ReducerExample from './ReducerExample'
 import CacheExample from './CacheExample'
-import MobxExample from './MobxExample'
 import JsxLiteralExample from './JsxLiteralExample'
+import LottieExample from './LottieExample'
+import MobxExample from './MobxExample'
 import HttpExample from './HttpExample'
 import WebViewExample from './WebViewExample'
 
 export default {
   ClassExample,
-  ReducerExample,
   CacheExample,
-  MobxExample,
+  LottieExample,
   JsxLiteralExample,
+  MobxExample,
   HttpExample,
   WebViewExample
 }
