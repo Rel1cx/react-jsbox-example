113acceba5f6232f0d8e6269975a4588d8900830
Nicify <nsnull@protonmail.com>
2020-06-26 16:23:45 +0800

    feat: add AnimateExample

--- src/scripts/examples/index.js
+++ src/scripts/examples/index.js
@@ -2,6 +2,7 @@ import BasicExample from './BasicExample'
 import CacheExample from './CacheExample'
 import JsxLiteralExample from './JsxLiteralExample'
 import TodoExample from './TodoExample'
+import AnimateExample from './AnimateExample'
 import LottieExample from './LottieExample'
 import HttpExample from './HttpExample'
 import WebViewExample from './WebViewExample'
@@ -11,6 +12,7 @@ export default {
   CacheExample,
   JsxLiteralExample,
   TodoExample,
+  AnimateExample,
   LottieExample,
   HttpExample,
   WebViewExample,
