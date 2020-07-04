bd0eaaa68271a11b808490d2f61c371ef391bc03
Nicify <nsnull@protonmail.com>
2020-07-05 02:43:22 +0800

    fix: HttpExample not show error and loading state

--- src/scripts/examples/HttpExample.js
+++ src/scripts/examples/HttpExample.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useMemo } from 'react'
+import React from 'react'
 import useSWR from 'swr'
 import { apiKey } from '../constants'
 import { articleToMarkdown } from '../helper'
@@ -8,35 +8,35 @@ const { width } = $device.info.screen
 async function fetcher(url) {
   const { data, error } = await $http.get(url)
   if (error) {
-    throw error
+    throw new Error(error.localizedDescription)
   }
   return data
 }
 
-const Loading = () => {
-  return (
-    <>
-      <label frame={styles.loading} font={$font(48)} text="APOD" align={$align.center} />
-      <spinner frame={styles.spinner} loading={true} />
-    </>
-  )
-}
-
-const Content = () => {
-  const { data } = useSWR(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, fetcher, {
-    suspense: true,
-  })
-
-  const content = useMemo(() => articleToMarkdown(data.url, data.title, data.explanation), [data])
-
-  return <markdown frame={styles.content} content={content} />
-}
-
 export default function HttpExample() {
+  const { data, error } = useSWR(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`, fetcher)
+
+  // Error state
+  if (error) {
+    return <label frame={styles.loading} align={$align.center} lines={5} font={$font(16)} text={error.message} />
+  }
+
+  // Loading state
+  if (!data) {
+    return (
+      <>
+        <label frame={styles.loading} font={$font(48)} text="APOD" align={$align.center} />
+        <spinner frame={styles.spinner} loading={true} />
+      </>
+    )
+  }
+
+  // Fetched content state
   return (
-    <Suspense fallback={Loading}>
-      <Content />
-    </Suspense>
+    <markdown
+      frame={styles.content}
+      content={articleToMarkdown(data.url, data.title, data.explanation)}
+    />
   )
 }
 
